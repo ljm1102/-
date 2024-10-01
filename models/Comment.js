bc6d79089cf8b01b607c0506712b6d6acbef026e
ljm1102 <127467536+ljm1102@users.noreply.github.com>
2024-10-01 23:39:21 +0900

    ADD comment schema

---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const commentSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Post' // 댓글이 속한 게시글과 연결
+    },
+    nickname: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    commentPassword: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// 비밀번호 저장 전 해시 처리
+commentSchema.pre('save', async function(next) {
+    if (this.isModified('commentPassword')) {
+        this.commentPassword = await bcrypt.hash(this.commentPassword, 10);
+    }
+    next();
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
